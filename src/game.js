0193d297022c6f736f2bb9de7189b37748892aa8
Adelpozo04 <addelp01@ucm.es>
2023-10-20 17:45:11 +0200

    Update game.js

--- src/game.js
+++ src/game.js
@@ -8,15 +8,3 @@ var config = {
 }
 
 new Phaser.Game(config) //constructor de phaser en si
-
-function create(){
-
-    //setOrigin para cambiar el origen respecto a la imagen y setScale para cambiar el tamaño
-    this.add.image(200, 300, "samuel").setOrigin(0, 0).setScale(0.1, 0.1);
-}
-
-function preload(){
-    //hace falta una clave que es el nombre y una ruta
-    this.load.image("samuel","Images/orangutan.png");
-
-}
