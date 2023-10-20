ba25000bbd88caf7d9b41fa5789ee2a8c5d9ec09
Adelpozo04 <addelp01@ucm.es>
2023-10-20 18:10:33 +0200

    Cambio en el index despejandolo

--- src/game.js
+++ src/game.js
@@ -1,9 +1,12 @@
-import PantallaInicial from "PantallaInicial.js"
+import PantallaInicial from "./PantallaInicial.js"
 
 var config = {
     type: Phaser.AUTO,
     width: 1200,
     height: 800,
+    scale: {
+		autoCenter: Phaser.Scale.CENTER_HORIZONTALLY
+	},
     pixelArt:true,
     scene : [PantallaInicial]
 
