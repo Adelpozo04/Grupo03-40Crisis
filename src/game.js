2b93a6d5b1783b84030e11c70f11e68de0da7b67
Adelpozo04 <addelp01@ucm.es>
2023-10-20 18:27:17 +0200

    Cambio pantalla tamaño

--- src/game.js
+++ src/game.js
@@ -2,8 +2,8 @@ import PantallaInicial from "./PantallaInicial.js"
 
 var config = {
     type: Phaser.AUTO,
-    width: 1200,
-    height: 800,
+    width: 1100,
+    height: 700,
     scale: {
 		autoCenter: Phaser.Scale.CENTER_HORIZONTALLY
 	},
