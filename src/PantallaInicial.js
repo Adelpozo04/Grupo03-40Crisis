f55097a6c6a23e0aeacc662d633a767facbc5008
Adelpozo04 <addelp01@ucm.es>
2023-10-24 00:24:28 +0200

    Cargar la fuente del texto y aplicarla a la pantalla de inicio

--- src/PantallaInicial.js
+++ src/PantallaInicial.js
@@ -1,3 +1,4 @@
+
 export default class PantallaInicial extends Phaser.Scene{
 
     constructor(){
@@ -12,10 +13,36 @@ export default class PantallaInicial extends Phaser.Scene{
 
         console.log();
     }
+
+    loadFont(name, url) {
+		let self = this;
+	    let newFont = new FontFace(name, `url(${url})`);
+	    newFont.load()
+	    // Función que se llamará cuando las fuentes estén cargadas
+	    // en este caso, load devuelve lo que llamamos una promesa
+	    // más info en: https://developer.mozilla.org/en-US/docs/Web/API/FontFace/load
+	    .then(function (loaded) { 
+	        document.fonts.add(loaded);
+	        self.continueCreate();
+	    }).catch(function (error) {
+	        return error;
+    	});
+	}
     
     create(){
-        console.log("create");
+        this.loadFont("TitleFont", "../assets/fonts/RUBBBB__.TTF");
        
+    }
+
+    continueCreate() {
+			
+        this.GenerateText(this.cameras.main.centerX, 250, '40 CRISIS', 'PantallaInicial');
+	}
+
+    GenerateText(x, y, message, sceneKey){
+		let text = this.add.text(x, y, message, 
+            { fontFamily: 'TitleFont', fontSize: 90, color: 'white' })
+        text.setOrigin(0.5,0.5);		
     }
     
     update(t, dt){
