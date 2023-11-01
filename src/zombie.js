97f2d5c08591ac0c80ecb26d3919ed22bdedee6a
Miguelale23 <miguelnp@ucm.es>
2023-11-01 17:43:01 +0100

    prototipo zombie

--- src/zombie.js
+++ src/zombie.js
@@ -13,11 +13,13 @@ constructor(scene, x, y, key){
     this.dirY = 0;
     this.speed = 1;
 
+    this.mikeX = 0;
+    this.mikeY = 0;
 
     this.zombie = new Phaser.GameObjects.Sprite(scene, 0, 0, key, 0);
     
     this.add(this.zombie);
-
+    this.setScale(0.25);
     this.scene.add.existing(this);
 
     this.scene.anims.create({
@@ -37,8 +39,29 @@ constructor(scene, x, y, key){
     this.key = key;
 }
 
-preupdate(t, dt){
-    this.zombie.preupdate(t,dt);
-    this.zombie.play('walk' + this.key, true);
+updateMikeValues(x, y)
+{
+    this.mikeX = x;
+    this.mikeY = y;
+}
+preUpdate(t, dt){
+    this.zombie.preUpdate(t,dt);
+
+    if (this.x < this.mikeX)
+        this.dirX = 1;
+    else if (this.x > this.mikeX)
+        this.dirX = -1;
+    if (this.y < this.mikeY)
+        this.dirY = 1;
+    else if (this.y > this.mikeY)
+        this.dirY = -1;
+
+
+    this.x += this.speed * this.dirX;
+    this.y += this.speed * this.dirY;
+    if (this.dirX == 0 && this.dirY == 0)
+        this.zombie.play('idle' + this.key, true);
+    else
+        this.zombie.play('walk' + this.key, true);
 }
 }
