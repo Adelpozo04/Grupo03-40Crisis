6949bf3ceae8f5039ec08730dff2f2f2e35986de
itsJ1mbo <javicentear@gmail.com>
2023-11-17 18:44:14 +0100

    inicio arma.js

--- src/arma.js
+++ src/arma.js
@@ -1,3 +1,25 @@
-export default class Arma {
-    
+export default class Arma extends Phaser.GameObjects.Sprite {
+    /**
+     * @param {number} daño - daño del arma
+     * @param {number} cadencia - velocidad de disparo
+     * @param {number} maxAmmo - municion del arma
+     * @param {number} actualAmmo - municion actual del arma
+     * @param {playerContenedor} player - referencia al player
+     */ 
+
+    constructor(daño, cadencia, ammo){
+        this.daño = daño;
+        this.cadencia = cadencia;
+        this.maxAmmo = ammo;
+        
+        this.añadeMunicion();
+    }
+
+    ataca(){
+        --this.actualAmmo;
+    }
+
+    añadeMunicion(){
+        this.actualAmmo = this.maxAmmo;
+    }
 }
