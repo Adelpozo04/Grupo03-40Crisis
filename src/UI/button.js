2697c2d685ef8954bb43e828e962d339e6d0ee08
Adelpozo04 <addelp01@ucm.es>
2023-12-20 03:07:06 +0100

    ultima comentada de codigo

--- src/UI/button.js
+++ src/UI/button.js
@@ -2,6 +2,18 @@
 
 export default class button extends Phaser.GameObjects.Container{
 
+/**
+ * 
+ * @param {*} scene 
+ * @param {*} x 
+ * @param {*} y 
+ * @param {*} key 
+ * @param {*} xReduction //cuanto debe ir a la derecha la zona de interaccion del boton
+ * @param {*} yReduction //cuanto debe bajar la zona de interaccion del boton
+ * @param {*} width //el ancho de la zona interactiva
+ * @param {*} height //el alto de la zona interactiva
+ */
+
     constructor(scene, x, y, key, xReduction, yReduction, width, height){
 
         super(scene, x, y);
@@ -22,6 +34,7 @@ export default class button extends Phaser.GameObjects.Container{
 
     }
 
+    //flipea el sprite que componga el boton segun lo que establezca el jugador(funciona como el de phaser)
     setFlip(flipX, flipY){
 
         this.flipX = flipX;
